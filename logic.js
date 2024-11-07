5df39fc92eb09fdf38ec161fc4482df3497d783e
PRANESH KARTHI M S <107921577+mspk5196@users.noreply.github.com>
2024-11-07 14:04:47 +0530

    Modified style

--- logic.js
+++ logic.js
@@ -1,30 +1,55 @@
-const text=document.getElementById("text");
+const text = document.getElementById("text");
 
-function appendtotext(input){
-    text.value+=input;
+function appendtotext(input) {
+    text.value += input;
 }
 
-function clearText(){
-    text.value="";
+function clearText() {
+    text.value = "";
 }
-function cal(){
-    try{
-        text.value=eval(text.value);
+function cal(event) {
+    let flag = 0;
+
+    try {
+        text.value = eval(text.value);
     }
-    catch(error)
-    {
-        text.value="Error";
+    catch (error) {
+        text.value = "Error";
         alert("Please Enter numbers and correct arithmetic symbols");
-        flag=1;
+        flag = 1;
     }
-    if(flag==1){
-        if(text.value=="Error"){
-            if(confirm('Please clear the text field and enter, if you want to clear it automatically ?')==true){
-                text.value="";
+    if (flag == 1 && text.value == "Error" || text.value == "undefined") {
+
+        if (confirm('Please clear the text field and enter or type anything, if you want to clear it automatically ?') == true) {
+            text.value = "";
+        }
+        else {
+            alert("Clear text manually");
+        }
+
+    }
+    
+}
+text.addEventListener("keydown",function(event){
+    let flag = 0;
+    if (event.key === "Enter") {
+        try {
+            text.value = eval(text.value);
+        }
+        catch (error) {
+            text.value = "Error";
+            alert("Please Enter numbers and correct arithmetic symbols");
+            flag = 1;
+        }
+        if (flag == 1 && text.value == "Error" || text.value == "undefined") {
+
+            if (confirm('Please clear the text field and enter or type anything, if you want to clear it automatically ?') == true) {
+                text.value = "";
             }
-            else{
+            else {
                 alert("Clear text manually");
             }
+
         }
     }
-}
+})
